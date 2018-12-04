e7e72887ed855e63663cf2e0f156dcc1178dd727
matal3a0 <johan@bjornell.se>
2018-12-04 21:05:27 +0100

    Day 4

---
+++ day4_2.py
@@ -0,0 +1,33 @@
+#!/bin/python3
+
+import sys,re,operator
+from collections import defaultdict,Counter
+
+def main():
+    data = []
+    guards = []
+    schedule = defaultdict(list)
+    
+    for line in sys.stdin:
+        data.append(re.findall(r"[\w']+", line))
+    data.sort()
+    
+    for d in data:
+        if d[5] == 'Guard':
+            guard = int(d[6])
+        elif d[5] == 'falls':
+            startsleep = int(d[4])
+        elif d[5] == 'wakes':
+            endsleep = int(d[4])
+            for minute in range(startsleep,endsleep):
+                schedule[guard].append(minute)
+    
+    for k, v in schedule.items():
+        count = dict(Counter(v))
+        bestminute = max(count.items(), key=operator.itemgetter(1))[0]
+        guards.append([count[bestminute],k,bestminute])
+    
+    best = max(guards)
+    print(best[1]*best[2])
+
+main()
