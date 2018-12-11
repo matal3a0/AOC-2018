ff39af948fdc86bbf9a44ab716fe7bb30b1b9a62
matal3a0 <johan@bjornell.se>
2018-12-11 12:37:17 +0100

    Refactored Day 5

--- day5.py
+++ day5.py
@@ -1,19 +1,19 @@
 #!/bin/python3
 
+import string
+
 def reduce(d):
     matchfound = True
 
     while matchfound:
-        i = 0
         s = len(d)
         matchfound = False
-        while(i < s-1):
-            if not d[i] == d[i+1]:
-                if d[i].lower() == d[i+1] or d[i].upper() == d[i+1]:
-                    matchfound = True
-                    d = d[:i] + d[i+2:]
-            i = (i + 1) % s
-            s = len(d)
+        for c in string.ascii_lowercase:
+            d = d.replace(c+c.upper(),'')
+            d = d.replace(c.upper()+c,'')
+            #print(d)
+            if len(d) < s:
+                matchfound = True
     return d
 
 def main():
